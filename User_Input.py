0b20634bc330e8b0f23ffecb799bad1673e62198
Abraham Oliver <160440580+username121234@users.noreply.github.com>
2024-03-08 20:16:30 +0300

    Add files via upload

---
+++ User_Input.py
@@ -0,0 +1,23 @@
+print("Greetings good fella!!!")
+name = input("What is your name?")
+age = input("what is your age? ")
+location = input("Where are you from? ")
+
+
+def main():
+    name=(input(name))
+    age=(input(age))
+    location=(input(location))
+    
+print("Hey",(name)+", you are "+(age) +" years old and live in "+(location)+(('.')))
+
+
+
+
+
+
+
+
+
+
+
